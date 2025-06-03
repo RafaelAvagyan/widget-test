3496f19d3400a82f5cb68e4a01f1d865f924207e
RafaelAvagyan <rafael.avagyan.97@mail.ru>
2025-06-03 17:23:12 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react'
+
+
+function App() {
+    useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://telegram.org/js/telegram-widget.js?22';
+    script.async = true;
+    script.setAttribute('data-telegram-login', 'CreateTodoBot');
+    script.setAttribute('data-size', 'large');
+    script.setAttribute('data-auth-url', 'https://tg-bot-interface.vercel.app/');
+    script.setAttribute('data-request-access', 'write');
+    
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, [])
+  return <div id="telegram-login-container"></div>
+}
+
+export default App
